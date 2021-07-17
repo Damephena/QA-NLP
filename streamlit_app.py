3c358548a033cda8d10240a085e587bba2893a27
damephena <okoyeifenna24@gmail.com>
2021-07-17 17:10:57 +0100

    minor app clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,49 +46,62 @@ def get_wiki_paragraph(query):
         return wikipedia.summary(ambiguous_terms[0], chars=384)
     return summary
 
-if __name__ == "__main__":
+def answer_search(context, question):
 
+    '''Animated spinner during model prediction'''
+    try:
+        with st.spinner(text='Searching for answer...🧐👀⌚⏳⌛'):
+            answer = answer_question(context, question)
+            if answer != 'empty':
+                st.success(answer)
+            else:
+                st.info('Sorry, I do not have any answer to this question')
+    except Exception as e:
+        st.warning('You must provide a valid original paragraph')
+
+def check_question(context, question):
+
+    '''Checks that the question textbox is not empty.'''
+    if question != '':
+        answer_search(context, question)
+    else:
+        st.error('Ask me a question.')
+
+def main():
+    subheader_1 = '''
+    This is a simple user interface that implements the custom Question Answering model by Ifenna.
+    - Provide a context for your question; *original text* or *using wikipedia.*
+    - Ask a question based on the context you provided.
+    - Wait for a response from the system which is predicted by the custom model.
+    ---
+    '''
     title_slot = st.title('Information retrieval')
+    subheader_slot = st.markdown(subheader_1)
     paragraph_slot = st.empty()
-    help_text = 'Provide a context for your question'
-    original_context = st.text_area('Original Text', height=3, help=help_text)
-    question = st.text_input("QUESTION", help='Ask a question', max_chars=128)
+    question = st.text_input('QUESTION', help='Ask a question', max_chars=128)
 
     if st.checkbox('Use Wikipedia'):
-        wiki_query = st.text_input("WIKIPEDIA SEARCH TERM", "")
+        wiki_query = st.text_input('WIKIPEDIA SEARCH TERM', '')
 
         if wiki_query:
-            wiki_para = get_wiki_paragraph(wiki_query)
+            wiki_para = get_wiki_paragraph(f'---\n{wiki_query}\n ---')
             paragraph_slot.markdown(wiki_para)
             title_slot.markdown('# Wikipedia Article')
+            subheader_slot.markdown('*Article sourced from Wikipedia*')
+            check_question(wiki_para, question)
+        else:
+            st.error('Kindly provide a valid Wikipedia term to search.')
 
-            # Execute question against paragraph
-            if question != '':
-                try:
-                    with st.spinner(text='Searching for answer...🧐👀⌚⏳⌛'):
-                        answer = answer_question(wiki_para, question)
-                        if answer:
-                                st.success(answer)
-                        else:
-                                st.info('Sorry, I do not have any answer to this question')
-                except Exception as e:
-                    print(e)
-                    st.warning("You must provide a valid wikipedia paragraph")
-            else:
-                st.warning('Kindly provide a question')
+    else:
+        help_text = 'Provide a context for your question'
+        original_context = st.text_area('Original Text', height=4, help=help_text)
+        if original_context != '':
+            paragraph_slot.markdown(f'---\n{original_context}\n ---')
+            title_slot.markdown('# Original Article')
+            subheader_slot.markdown('*Your Original article*')
+            check_question(original_context, question)
+        else:
+            st.error('Kindly provide a context (original text) for your question')
 
-    elif original_context:
-        paragraph_slot.markdown(original_context)
-        title_slot.markdown('# Original Article')
-        # Execute question against paragraph
-        if question != "":
-            try:
-                with st.spinner(text='Searching for answer...🧐👀⌚⏳⌛'):
-                    answer = answer_question(original_context, question)
-                    if answer:
-                        st.success(answer)
-                    else:
-                        st.info('Sorry, I do not have any answer to this question')
-            except Exception as e:
-                print(e)
-                st.warning("You must provide a valid wikipedia paragraph")
+if __name__ == '__main__':
+    main()
